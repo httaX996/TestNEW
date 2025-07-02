d54dc28cad388c526305ad3f42cf300982e6e72f
kavindu-12 <kavindukalpana72@gmail.com>
2025-07-02 20:09:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'KAVIYA-MD~Vy02TQ7a#jK8yU6ghWQEZtvV8HSnTk1ZX3Q_k61QCV1TZQQS5d7w,
+SESSION_ID: process.env.SESSION_ID || 'KAVIYA-MD~bhB2yAqa#qZ4_8jxHHK40o8s2BxrMC_qs1ouGDWdzCB8-KhiycEg',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94705310919', 
 PREFIX:  process.env.PREFIX || ['.'] ,
