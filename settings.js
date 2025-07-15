7c13a206e36eb22e46951e42aa8c3d1e7087f307
Nano-999 <thaktharikita@gmail.com>
2025-07-15 17:14:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'KAVIYA-MD~bhB2yAqa#qZ4_8jxHHK40o8s2BxrMC_qs1ouGDWdzCB8-KhiycEg',
+SESSION_ID: process.env.SESSION_ID || 'KAVIYA-MD~flhmiDhT#Hu6kCLSXBUIaATxZwNA3B805I-QpXv3h4XXHoHHs320g',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgresql://postgres:mQIupvWlkxQkvRlXNphQinHDiUZBRuds@caboose.proxy.rlwy.net:25715/railway',
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94705310919', 
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94779510013', 
 PREFIX:  process.env.PREFIX || ['.'] ,
 LOGO: process.env.LOGO || `https://imgur.com/a/aRAoEaR`,
 OWNER_REACT: process.env.OWNER_REACT || 'true',
-AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
+AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "false",
 ANTI_CALL: process.env.ANTI_CALL || "true"
 };
